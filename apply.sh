0b323ba05ed8f24019eaf0d8f4360fb0f4bac77f
Rafael Arantes <birutaibm@gmail.com>
2020-01-11 10:34:48 -0300

    Add some installs

--- apply.sh
+++ apply.sh
@@ -5,18 +5,42 @@ if ! [ $(id -u) = 0 ]; then
    exit 1
 fi
 
-# install google chrome
+# basic packages
+apt install -y curl
+apt-get install -y python-software-properties
+apt install -y snapd
+
+# configure apt
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | tee /etc/apt/sources.list.d/google-chrome.list
+curl -sL https://deb.nodesource.com/setup_13.x | bash -
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg |  apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 apt update
-apt install -y google-chrome-stable
 
-apt install -y curl
+# install
+apt install -y google-chrome-stable
 apt install -y openjdk-11-jdk
-apt-get install eclipse
-apt-get install git
+apt-get install -y eclipse
+apt-get install -y maven
+apt-get install -y git
+snap install node --classic
+apt-get -o Dpkg::Options::="--force-overwrite" install -y yarn
+snap install -y code --classic
+snap install insomnia
+apt install -y fonts-firacode
+npm install -g eslint
+npm install -g expo-cli
+apt install -y zsh
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# init current dir as config-pack git repository
 git init
 git remote add github https://github.com/birutaibm/config-pack.git
 
+
 # create .gitconfig
 ln -rs ./.gitconfig ~/.gitconfig
+ln -rs vscode/settings.json ~/.config/Code/User/settings.json
+
